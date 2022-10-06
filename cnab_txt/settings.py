590117a6e4de733dccc00a86b68d35922fad2103
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-10-06 13:17:47 -0300

    altered settings and requirements.txt

--- cnab_txt/settings.py
+++ cnab_txt/settings.py
@@ -170,9 +170,3 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'CNAB TXT',
-    'DESCRIPTION': 'fazendo upload de um arquivo de texto',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-}
